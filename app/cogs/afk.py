bd3014c2923ba3f72f4e9fe5220a12cfab0415f4
entity <65078619+entiddie@users.noreply.github.com>
2021-02-02 23:10:35 +0530

    Update afk.py

--- app/cogs/afk.py
+++ app/cogs/afk.py
@@ -24,13 +24,7 @@ class Afk(commands.Cog):
 
         if len(status) > 100:
             await ctx.send(f"Status cannot have more than 100 characters")
-
-        elif "<@" in status:
-            await ctx.send(f"Status cannot contain mentions")
-
-        elif "@everyone" in status or "@here" in status:
-            await ctx.send(f"Status cannot contain mentions")
-
+            
         else:
             users = await get_afk_data()
 
@@ -46,26 +40,7 @@ class Afk(commands.Cog):
                 json.dump(users, f)
 
             await ctx.send(f"AFK status set.")
-
-    
-    @commands.command(aliases=['imback'])
-    async def removeafk(self, ctx):
-        try:
-            users = await get_afk_data()
-
-            userid = str(ctx.author.id)
-                
-            del users[userid]
-
-            with open("afk.json", "w") as f:
-                json.dump(users, f)
-
-
-            await ctx.send(f"Welcome back {ctx.author.display_name}, I removed your AFK status")
-
-        except:
-            await ctx.send(f"You have not set an AFK status. Try <afk (status)")
-
+            
         
 def setup(client):
     client.add_cog(Afk(client))
